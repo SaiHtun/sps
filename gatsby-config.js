9fefd8ad9ce6712a5e2239675db237e3fc140772
saihtun <saihtun1430@gmail.com>
2021-01-08 17:33:57 -0800

    :pray: gg

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
